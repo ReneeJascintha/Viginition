ed75c91e6aa956562cfc974b9ba330229e828022
Anvita <kumaranvita30@gmail.com>
2025-02-01 02:48:12 +0530

    init

--- vigini/src/App.js
+++ vigini/src/App.js
@@ -1,36 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import Home from "./components/home";
-import "./styles.css";
 import SearchEnginePage from './components/searchresults';
+import "./styles.css";
 
 const App = () => {
-  const imageUrl = "https://source.unsplash.com/random/1600x900"; // Example: Dynamic Unsplash Image
+  const imageUrl = "../public/assets/images/bg-image.jpg"; 
   return (
     <div>
       <Home />
