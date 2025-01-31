1c17684b57d0dfe1a45b8405e5732fc0cf4428ee
Anvita <kumaranvita30@gmail.com>
2025-02-01 01:13:17 +0530

    init

--- vigini/src/components/searchbar.js
+++ vigini/src/components/searchbar.js
@@ -3,22 +3,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBar = () => {
-  const [selectedOption, setSelectedOption] = useState("All");
   return (
     <div className="search-container">
-      <select 
-        className="dropdown" 
-        value={selectedOption} 
-        onChange={(e) => setSelectedOption(e.target.value)}
-      >
-        <option value="All">All</option>
-        <option value="News">News</option>
-        <option value="Images">Images</option>
-        <option value="Videos">Videos</option>
-      </select>
+      <input type="text" className="search-box" placeholder="Search..." />
       
-      <input type="text" className="search-box" placeholder={`Search in ${selectedOption}...`} />
-
       <button className="search-btn">
         <FontAwesomeIcon icon={faSearch} />
       </button>
