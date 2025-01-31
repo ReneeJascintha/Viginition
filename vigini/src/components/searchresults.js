e384cbbdfe5c7ec1e083f69e84796d26437ea58d
Anvita <kumaranvita30@gmail.com>
2025-02-01 02:30:58 +0530

    init

--- vigini/src/components/searchresults.js
+++ vigini/src/components/searchresults.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { Card, CardContent } from "../ui/card";
-// import { Button } from "../ui/button";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
