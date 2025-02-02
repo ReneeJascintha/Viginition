3076914b60021b66e4ee557dbce68bb33bb72123
Anvita <kumaranvita30@gmail.com>
2025-02-03 02:32:00 +0530

    init

--- vigini/src/components/DynamicBackground.js
+++ vigini/src/components/DynamicBackground.js
@@ -28,7 +28,7 @@ const DynamicBackground = () => {
         transition: "background-image 1s ease-in-out",
       }}
     >
-      <h1 className="text-white text-3xl font-bold">Welcome to My Website</h1>
+      {/* <h1 className="text-white text-3xl font-bold">Welcome to My Website</h1> */}
     </div>
   );
 };
