4cf6037038f7ec8f8fad6cc8e02de52937989b57
Renee David <102434516+ReneeJascintha@users.noreply.github.com>
2025-02-03 22:38:02 +0530

    init12

--- vigini/src/components/home.js
+++ vigini/src/components/home.js
@@ -4,15 +4,16 @@ import SearchBar from "./searchbar";
 const Home = ({ setIsSearching }) => {
   return (
     <div className="home">
-      <div className="content">
+      
         <SearchBar setIsSearching={setIsSearching} />
-        <img src="" alt="noo" />
+        <div className="content">
+        <div className="logo"></div></div>
         <div class="tagline">Precision in Every Search!</div>
         <div className="news-section">
           <h2>News Section</h2>
         </div>
       </div>
-    </div>
+    
   );
 };
 
